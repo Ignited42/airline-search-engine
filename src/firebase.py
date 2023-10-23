b1f4e1a45e95a726117c89984bd061b90185a3a2
Steven Subianto <steven.subianto@wsu.edu>
2023-10-22 20:04:11 -0700

    Changed the credentials filename in firebase.py

--- src/firebase.py
+++ src/firebase.py
@@ -2,5 +2,18 @@
 # Description:
 #   Functions described here are designed to interface with the Realtime DB.
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('../credentials.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://flighttoolapp-default-rtdb.firebaseio.com'
+})
+
 def create_collection(dataframe, collection_name):
+
     return
