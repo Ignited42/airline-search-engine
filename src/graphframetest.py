3a2f58d7675666ee450db5433f6af85e63f7b09c
Yuuki Matsunari <yuuki.matsunari@wsu.edu>
2023-12-07 22:58:17 -0800

    fixed code issues

--- src/graphframetest.py
+++ src/graphframetest.py
@@ -4,10 +4,10 @@ from graphframes import *
 spark = SparkSession.builder.getOrCreate()
 
 airports = spark.read.csv(r"newdata\airports.csv", header=True)
-routes = spark.read.csv(r"C:newdata\routes.csv", header=True)
+routes = spark.read.csv(r"newdata\routes.csv", header=True)
 
-vertices = airports.withColumnRenamed("IATA", "id")
-edges = routes.withColumnRenamed("Source airport ID", "src").withColumnRenamed("Destination airport ID", "dst")
+vertices = airports.withColumnRenamed("IATA", "airport")
+edges = routes.withColumnRenamed("Source airport", "src").withColumnRenamed("Destination airport", "dst")
 
 g = GraphFrame(vertices, edges)
 
