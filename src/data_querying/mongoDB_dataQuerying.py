57695bc580fbcd289fc74590b66ec9be00e9780b
Steven Subianto <steven.subianto@wsu.edu>
2023-12-07 19:47:11 -0800

    Added comments to dataQuerying functions

--- src/data_querying/mongoDB_dataQuerying.py
+++ src/data_querying/mongoDB_dataQuerying.py
@@ -9,6 +9,7 @@ import pymongo
 import pprint
 from pyspark.sql import SparkSession
 
+# Connecting to MongoDB using certificate
 uri = "mongodb+srv://cluster0.pywaf93.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
 client = pymongo.MongoClient(uri,
                      tls=True,
@@ -17,15 +18,27 @@ client = pymongo.MongoClient(uri,
 
 db = client["FlightToolApp"]
 
-"""spark = SparkSession.builder.appName("FlightToolApp") \
-                    .config("spark.mongodb.input.connection.uri", uri) \
-                    .config("spark.mongodb.output.connection.uri", uri) \
-                    .getOrCreate()"""
+# Connecting to MongoDB using password
+"""
+uriSpark = "mongodb+srv://developer-2:HXxTrHl8gcVbLjSH@cluster0.pywaf93.mongodb.net/?retryWrites=true&w=majority"
+spark = SparkSession.builder.appName("FlightToolApp") \
+                    .config("spark.mongodb.read.connection.uri", uriSpark) \
+                    .config("spark.mongodb.write.connection.uri", uriSpark) \
+                    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.2.1") \
+                    .getOrCreate()
+"""
+# ========================================================================
 
 def getCollection(collectionName):
+    """
+    Retrieves a collection by its name from the remote MongoDB.
+
+     - collectionName: name of a valid collection
+    """
     try:
         #Spark implementation
-        """dataFrame = spark.read.format("mongodb") \
+        """ 
+        dataFrame = spark.read.format("mongodb") \
                     .option("database", "FlightToolApp") \
                     .option("collection", collectionName).load()"""
         
@@ -37,6 +50,11 @@ def getCollection(collectionName):
     return collection
 
 def listAirportsInCountry(countryName):
+    """
+    Returns a List of airport rows to a given country, by
+    retrieving from MongoDB.
+    """
+    
     # First, check if country exists
     countryEntry = db["Countries"].find_one({"Name": countryName})
     airportsList = []
@@ -55,7 +73,11 @@ Reference code:
 https://stackoverflow.com/questions/18501064/mongodb-aggregation-counting-distinct-fields
 https://stackoverflow.com/questions/24761266/select-group-by-count-and-distinct-count-in-same-mongodb-query/24770233#24770233
 """
-def getBusiestCountry():
+def listBusiestCountries():
+    """
+    Returns a list of countries sorted by their 
+    """
+
     # Aggregate data to get busiest country by number of airports
     busyList = []
 
