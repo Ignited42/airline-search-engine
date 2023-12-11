ef5f2510e46364856209a640916961362f061a5c
Steven Subianto <steven.subianto@wsu.edu>
2023-12-11 11:13:05 -0800

    Added missing content that was lost in the merge
    Content that was missing belonged to the back-end portion of the demo.

---
+++ src/demo.py
@@ -0,0 +1,7 @@
+# Authors: Mark, Steven, Yuuki
+# Description:
+#   Main file to run source code
+
+from utils import menu
+
+menu.run_main_menu()
--- src/utils/print_contents.py
+++ src/utils/print_contents.py
@@ -1,3 +1,45 @@
 # Author: Steven
 # Description:
 #   Prints out the contents of given items.
+
+def printAirport(airport):
+    """
+    Prints the contents of a document retrieved from Airports collection.
+    """
+    if airport == {}:
+        return
+
+    try:
+        print("Name: " + str(airport["Name"]) + ", " + str(airport["Code"]["IATA"]))
+        print("City: " + str(airport["City"]))
+        print("Country: " + str(airport["Country"]["Name"]))
+        print("Location:\n" + "\tLatitude: " + str(airport["Location"]["Latitude"]) +
+                            "\n\tLongitude: " + str(airport["Location"]["Longitude"]))
+    except Exception as e:
+        print(e)
+
+def printBusiestCountry(country):
+    """
+    Prints important content of countries given by listBusiestCountries().
+    """
+
+    try:
+        countryName = country["_id"]["Country"]
+        count = country["Count"]
+        print(f"{countryName} - {count}")
+    except Exception as e:
+        print(e)
+
+def printNHopsList(nHopsList):
+    """
+    Prints the contents of nHopsList, which is given by withinNHops().
+    """
+
+    try:
+        for hops in range(len(nHopsList)):
+            print(f"{hops + 1}: ")
+            for airport in nHopsList[hops + 1]:
+                print(f"\t{airport}")
+
+    except Exception as e:
+        print(e)
